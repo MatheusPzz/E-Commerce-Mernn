ca2e7889307c6b880354fd6225007a54b2dfe5a4
Matheus Perazzo <110614194+MatheusPzz@users.noreply.github.com>
2024-05-15 23:53:04 +0100

    Delete E-commerce-fullstack-main-main-main directory

--- E-commerce-fullstack-main-main-main/frontend/src/context/authContext.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const AuthContext = React.createContext({
-  token: null,
-  userId: null,
-  isAdmin: false,
-  login: (token, userId, isAdmin) => {},
-  logout: () => {},
-});
